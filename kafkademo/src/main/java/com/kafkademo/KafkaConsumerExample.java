c72cbae4522309433ce92f3ef7d36ec51bd818fb
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2025-01-30 12:07:45 +0530

    cdcfd

---
+++ kafkademo/src/main/java/com/kafkademo/KafkaConsumerExample.java
@@ -0,0 +1,29 @@
+package com.kafkademo;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConsumerExample {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test-group");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("batch3topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            records.forEach(record -> {
+                System.out.println("Received: " + record.value() + ", Partition: " + record.partition());
+            });
+        }
+    }
+}
---
+++ kafkademo/src/main/java/com/kafkademo/KafkaProducerExample.java
@@ -0,0 +1,24 @@
+package com.kafkademo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerExample {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++) {
+            producer.send(new ProducerRecord<>("batch3topic", "key-" + i, "message-" + i));
+        }
+
+        producer.close();
+    }
+}
